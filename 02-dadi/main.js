8bc9a583526e4a2bb740505d876f568e0a2b4da3
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-01-14 15:52:36 +0100

    add: numbers and if condition

--- 02-dadi/main.js
+++ 02-dadi/main.js
@@ -1,2 +1,19 @@
 // - generare un numero random da 1 a 6 (Math.random()*6)+1, sia per il giocatore sia per il computer
-// - tabilire il vincitore, in base a chi fa il punteggio più alto
+// - stabilire il vincitore, in base a chi fa il punteggio più alto
+
+// generatore numeri random per il giocatore
+let numberPlayer = ((Math.random()*5)+1).toFixed(0);
+console.log (parseInt(numberPlayer));
+
+// generatore numeri random per il computer
+let numberComputer = ((Math.random()*5)+1).toFixed(0);
+console.log (parseInt(numberComputer));
+
+// condizione if per determinare il vincitore
+if (numberPlayer == numberComputer) {
+    console.log (`Pari, il numero di entrambi i giocatori è uguale`)
+} else if (numberPlayer > numberComputer) {
+    console.log (`Il vincitore è il giocatore con il numero ${numberPlayer}`)
+} else {
+    console.log (`Il vincitore è il computer con il numero ${numberComputer}`)
+}
