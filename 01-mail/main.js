cecba9cfc9bf295d4c1efad06dec218fe4611271
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-01-14 12:20:06 +0100

    add: variables

---
+++ 01-mail/main.js
@@ -0,0 +1,14 @@
+// - crea una lista di email di invitati ad una festa (array)
+// - chiedi all’utente la sua email (promt)
+// - controlla che sia nella lista di chi può accedere (con if ==)
+// - stampa un messaggio appropriato sull’esito del controllo utilizzando un ciclo for
+// - non è consentito usare nessun metodo proprio degli array (come includes, per esempio)
+// - non è necessario provvedere alla validazione delle email
+
+let mail = ["valentino.rossi@mail.com",
+    "fabio.cannavaro@mail.com",
+    "giorgio.cognome@mail.com",
+    "fabio.caressa@mail.com",
+    "luca.toni@mail.com"
+]
+
